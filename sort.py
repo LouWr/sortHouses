d3805cae6aa3ec652001ff4457f6339faa101a35
LouisWright <louis--wright@hotmail.co.uk>
2023-03-08 13:27:58 +0000

    working gui - fixed issues with indexing col (not amazing fix but will do for now)

--- sort.py
+++ sort.py
@@ -2,15 +2,22 @@ import pandas as pd
 import chardet
 import re
 
+csv = 'house_list.csv'
+
 # chardet is package to detect encoding of file
-# this check encoding type to be passed to the pandas read_csv function
-with open('house_list.csv', 'rb') as f:
-    result = chardet.detect(f.read())  # or readline if the file is large
+# check encoding type and assign to result variable to use below.
+with open(csv, 'rb') as f:
+    result = chardet.detect(f.read())
 
 
 # pass the encoding to this function from above. 
 # error_bad_lines handles any bad formatting but this could cause issues with dataloss. Might be a smart way to format on input...
-data = pd.read_csv('data.csv', encoding=result['encoding'], on_bad_lines='skip')
+data = pd.read_csv(csv, encoding=result['encoding'], on_bad_lines='skip', index_col=False)
+
+
+
+# drop empty cols
+data = data.dropna(axis=1, how='all')
 
 # get the headers - store in List for mutability
 headers = list(data.columns.values)
@@ -47,23 +54,22 @@ def sort_by_price_low():
 
 
 # find the most expensive postcode
-# iloc is used to get the column by index number rather than by string as could be issues with spaces etc
 def find_most_expensive_postcode():
     # try except added to check code only applied to string values - error thrown on ints and code not necessary
     try:
         data[data.columns[price_index]] = data[data.columns[price_index]].apply(remove_non_numeric).astype(int)
     except:
         pass
-    # group by postcode and get the mean price
+    # group by postcode and get the mean price - multiple houses in same postcode might be sold at differnet price tags
     grouped = data.groupby(data[data.columns[postcode_index]])[data.columns[price_index]].mean()
     sorted_prices = grouped.sort_values(ascending=False)
     most_expensive_postcode = sorted_prices.index[0]
+    #convert to array to return first half below.
     first_characters_of_postcode = most_expensive_postcode.split()
     return f'The most expensive postcode is:\n {first_characters_of_postcode[0]}\n'
 
 
-
-
-print(sort_by_price_high())
-print(sort_by_price_low())
-print(find_most_expensive_postcode())
+print(data)
+# print(sort_by_price_high())
+# print(sort_by_price_low())
+# print(find_most_expensive_postcode())
---
+++ sort_gui.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+from tkinter import filedialog
+import chardet
+import pandas as pd
+import re
+
+# Create a Tkinter window
+window = tk.Tk()
+
+# Define a function to handle the "Open File" button click event
+def open_file():
+    # Use file dialog to allow user to select a CSV file
+    filepath = filedialog.askopenfilename(
+        filetypes=[("CSV Files", "*.csv")],
+        title="Select a CSV file"
+    )
+    if filepath:
+        # chardet is package to detect encoding of file
+        # check encoding type and assign to result variable to use below.
+        with open(filepath, 'rb') as f:
+            result = chardet.detect(f.read())
+        
+        # pass the encoding to this function from above. 
+        # error_bad_lines handles any bad formatting but this could cause issues with dataloss. Might be a smart way to format on input...
+        data = pd.read_csv(filepath, encoding=result['encoding'], on_bad_lines='skip', index_col=False)
+
+        # get the headers - store in List for mutability
+        headers = list(data.columns.values)
+        formatted_headers = [s.strip().lower() for s in headers]
+
+        # get the index of the headers we want to sort by
+        price_index = formatted_headers.index('price')
+        postcode_index = formatted_headers.index('postcode')
+        address_index = formatted_headers.index('address')
+
+        # Define functions to manipulate the data
+        def remove_non_numeric(string):
+            return re.sub(r'\D', '', string)
+
+        def sort_by_price_high():
+            # try except added to check code only applied to string values - error thrown on ints and code not necessary
+            try: 
+                data[data.columns[price_index]] = data[data.columns[price_index]].apply(remove_non_numeric).astype(int)
+            except:
+                pass
+            data_sorted_high = data.sort_values(data.columns[price_index], ascending=False)
+            return f'The 5 highest selling:\n {data_sorted_high.head(5).to_string(index=False)}\n'
+
+        def sort_by_price_low():
+            # try except added to check code only applied to string values - error thrown on ints and code not necessary
+            try:
+                data[data.columns[price_index]] = data[data.columns[price_index]].apply(remove_non_numeric).astype(int)
+            except:
+                 pass
+            data_sorted_low = data.sort_values(data.columns[price_index], ascending=True)
+            return f'The 5 lowest selling:\n {data_sorted_low.head(5).to_string(index=False)}\n'
+        
+        def find_most_expensive_postcode():
+            # try except added to check code only applied to string values - error thrown on ints and code not necessary
+            try:
+                data[data.columns[price_index]] = data[data.columns[price_index]].apply(remove_non_numeric).astype(int)
+            except:
+                pass
+            # group by postcode and get the mean price - multiple houses in same postcode might be sold at differnet price tags
+            grouped = data.groupby(data[data.columns[postcode_index]])[data.columns[price_index]].mean()
+            sorted_prices = grouped.sort_values(ascending=False)
+            most_expensive_postcode = sorted_prices.index[0]
+            #convert to array to return first half below.
+            first_characters_of_postcode = most_expensive_postcode.split()
+            return f'The most expensive postcode is:\n {first_characters_of_postcode[0]}\n'
+            
+        # Define functions to handle button clicks
+        def handle_high_click():
+            result_text.delete("1.0", tk.END) # clear previous result
+            result_text.insert(tk.END, sort_by_price_high())
+
+        def handle_low_click():
+            result_text.delete("1.0", tk.END) # clear previous result
+            result_text.insert(tk.END, sort_by_price_low())
+
+        def handle_most_expensive_postcode():
+            result_text.delete("1.0", tk.END)
+            result_text.insert(tk.END, find_most_expensive_postcode())
+
+        # Create buttons to manipulate the data
+        high_button = tk.Button(window, text="Sort by Price (High to Low)", command=handle_high_click)
+        high_button.pack()
+
+        low_button = tk.Button(window, text="Sort by Price (Low to High)", command=handle_low_click)
+        low_button.pack()
+
+        most_expensive_postcode_button = tk.Button(window, text="Find Most Expensive Postcode", command=handle_most_expensive_postcode)
+        most_expensive_postcode_button.pack()
+
+# Create a button to open the file dialog
+open_button = tk.Button(window, text="Open File", command=open_file)
+open_button.pack()
+
+# Create a text box to display the results
+result_text = tk.Text(window, height=50, width=100)
+result_text.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
